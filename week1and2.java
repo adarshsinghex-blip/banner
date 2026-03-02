b4ed8570d49a01429fe823a6c17b37868bb8ef4f
adarshsinghex-blip <adarshsinghex@gmail.com>
2026-03-02 09:00:42 +0530

    Add files via upload

---
+++ week1and2.java
@@ -0,0 +1,29 @@
+// Creating class with name DisplayResult
+class DisplayResult {
+
+    public static void main(String[] args) {
+
+        // create a string variable name and assign value Sam
+        String name = "Sam";
+
+        // Create an int variable rollNumber and assign value 1
+        int rollNumber = 1;
+
+        // Create a double variable percentMarks and assign value 99.99
+        double percentMarks = 99.99;
+
+        // Create a char variable result and assign value 'P' for pass
+        char result = 'P';
+
+        // Display the result
+        System.out.println(
+                "Displaying Result:\n" +
+                name + " with Roll Number " +
+                rollNumber + " has scored " +
+                percentMarks + "% marks and result is " +
+                result
+        );
+    }
+}
+
+
